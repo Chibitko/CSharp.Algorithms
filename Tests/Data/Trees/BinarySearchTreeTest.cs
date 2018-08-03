d2aa60511f133ffd9b94969f2fb08ef1e3c4a9e4
Sergey Chibitko <schibitko@themsteam.com>
2018-08-03 14:00:25 +0700

    remove copypasta from BinarySearchTreeTest

--- Tests/Data/Trees/BinarySearchTreeTest.cs
+++ Tests/Data/Trees/BinarySearchTreeTest.cs
@@ -29,89 +29,74 @@ namespace Tests.Data.Trees
         [Fact]
         public void Find()
         {
-            var root = new BinarySearchTree<int, int>();
-            Assert.True(root.Find(1000) == null);
-            root.Add(100, 100);
-            Assert.True(root.Find(100) != null);
-            root.Add(150, 150);
-            Assert.True(root.Find(150) != null);
-            root.Add(50, 50);
-            Assert.True(root.Find(50) != null);
-            root.Add(25, 25);
-            Assert.True(root.Find(25) != null);
-            root.Add(75, 75);
-            Assert.True(root.Find(75) != null);
-            root.Add(125, 125);
-            Assert.True(root.Find(125) != null);
-            root.Add(175, 175);
-            Assert.True(root.Find(175) != null);
+            var tree = new BinarySearchTree<int, int>();
+            Assert.True(tree.Find(1000) == null);
+            tree.Add(100, 100);
+            Assert.True(tree.Find(100) != null);
+            tree.Add(150, 150);
+            Assert.True(tree.Find(150) != null);
+            tree.Add(50, 50);
+            Assert.True(tree.Find(50) != null);
+            tree.Add(25, 25);
+            Assert.True(tree.Find(25) != null);
+            tree.Add(75, 75);
+            Assert.True(tree.Find(75) != null);
+            tree.Add(125, 125);
+            Assert.True(tree.Find(125) != null);
+            tree.Add(175, 175);
+            Assert.True(tree.Find(175) != null);
         }
 
         [Fact]
         public void Add()
         {
-            var root = new BinarySearchTree<int, int>();
-            Assert.True(root.Add(100, 100));
-            Assert.Equal("100", GetKeysAsCommaSeparatedString(root));
-            Assert.True(root.Add(150, 150));
-            Assert.Equal("100,150", GetKeysAsCommaSeparatedString(root));
-            Assert.True(root.Add(50, 50));
-            Assert.Equal("100,50,150", GetKeysAsCommaSeparatedString(root));
-            Assert.True(root.Add(25, 25));
-            Assert.Equal("100,50,150,25", GetKeysAsCommaSeparatedString(root));
-            Assert.True(root.Add(75, 75));
-            Assert.Equal("100,50,150,25,75", GetKeysAsCommaSeparatedString(root));
-            Assert.True(root.Add(125, 125));
-            Assert.Equal("100,50,150,25,75,125", GetKeysAsCommaSeparatedString(root));
-            Assert.True(root.Add(175, 175));
-            Assert.Equal("100,50,150,25,75,125,175", GetKeysAsCommaSeparatedString(root));
-            Assert.False(root.Add(175, 175));
+            var tree = new BinarySearchTree<int, int>();
+            Assert.True(tree.Add(100, 100));
+            Assert.Equal("100", GetKeysAsCommaSeparatedString(tree.Root));
+            Assert.True(tree.Add(150, 150));
+            Assert.Equal("100,150", GetKeysAsCommaSeparatedString(tree.Root));
+            Assert.True(tree.Add(50, 50));
+            Assert.Equal("100,50,150", GetKeysAsCommaSeparatedString(tree.Root));
+            Assert.True(tree.Add(25, 25));
+            Assert.Equal("100,50,150,25", GetKeysAsCommaSeparatedString(tree.Root));
+            Assert.True(tree.Add(75, 75));
+            Assert.Equal("100,50,150,25,75", GetKeysAsCommaSeparatedString(tree.Root));
+            Assert.True(tree.Add(125, 125));
+            Assert.Equal("100,50,150,25,75,125", GetKeysAsCommaSeparatedString(tree.Root));
+            Assert.True(tree.Add(175, 175));
+            Assert.Equal("100,50,150,25,75,125,175", GetKeysAsCommaSeparatedString(tree.Root));
+            Assert.False(tree.Add(175, 175));
         }
 
         [Fact]
         public void Remove()
         {
-            var root = new BinarySearchTree<int, int>();
-            root.Add(100, 100);
-            root.Add(150, 150);
-            root.Add(50, 50);
-            root.Add(25, 25);
-            root.Add(75, 75);
-            root.Add(125, 125);
-            root.Add(175, 175);
-            root.Add(10, 10);
-            root.Add(190, 190);
-            Assert.Equal("100,50,150,25,75,125,175,10,190", GetKeysAsCommaSeparatedString(root));
-            Assert.False(root.Remove(1000));
+            var tree = new BinarySearchTree<int, int>();
+            tree.Add(100, 100);
+            tree.Add(150, 150);
+            tree.Add(50, 50);
+            tree.Add(25, 25);
+            tree.Add(75, 75);
+            tree.Add(125, 125);
+            tree.Add(175, 175);
+            tree.Add(10, 10);
+            tree.Add(190, 190);
+            Assert.Equal("100,50,150,25,75,125,175,10,190", GetKeysAsCommaSeparatedString(tree.Root));
+            Assert.False(tree.Remove(1000));
             // if current has no right child
-            Assert.True(root.Remove(25));
-            Assert.Equal("100,50,150,10,75,125,175,190", GetKeysAsCommaSeparatedString(root));
+            Assert.True(tree.Remove(25));
+            Assert.Equal("100,50,150,10,75,125,175,190", GetKeysAsCommaSeparatedString(tree.Root));
             // if current's right child has no left child
-            root.Add(5, 5);
-            root.Add(15, 15);
-            Assert.Equal("100,50,150,10,75,125,175,5,15,190", GetKeysAsCommaSeparatedString(root));
-            Assert.True(root.Remove(10));
-            Assert.Equal("100,50,150,15,75,125,175,5,190", GetKeysAsCommaSeparatedString(root));
+            tree.Add(5, 5);
+            tree.Add(15, 15);
+            Assert.Equal("100,50,150,10,75,125,175,5,15,190", GetKeysAsCommaSeparatedString(tree.Root));
+            Assert.True(tree.Remove(10));
+            Assert.Equal("100,50,150,15,75,125,175,5,190", GetKeysAsCommaSeparatedString(tree.Root));
             // if current's right child has a left child
-            root.Add(145, 145);
-            Assert.Equal("100,50,150,15,75,125,175,5,145,190", GetKeysAsCommaSeparatedString(root));
-            Assert.True(root.Remove(100));
-            Assert.Equal("125,50,150,15,75,145,175,5,190", GetKeysAsCommaSeparatedString(root));
-        }
-
-        private static string GetKeysAsCommaSeparatedString(BinarySearchTree<int, int> root)
-        {
-            var bfs = new StringBuilder();
-            root.BreadthFirstSearch(node =>
-            {
-                if (bfs.Length > 0)
-                {
-                    bfs.Append(",");
-                }
-                bfs.Append(node.Value.Key);
-                return false;
-            });
-            return bfs.ToString();
+            tree.Add(145, 145);
+            Assert.Equal("100,50,150,15,75,125,175,5,145,190", GetKeysAsCommaSeparatedString(tree.Root));
+            Assert.True(tree.Remove(100));
+            Assert.Equal("125,50,150,15,75,145,175,5,190", GetKeysAsCommaSeparatedString(tree.Root));
         }
 
         private static string GetKeysAsCommaSeparatedString(BinarySearchTreeNode<int, int> root)
